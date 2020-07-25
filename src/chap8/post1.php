5d477edc0a81ff8b12d8886246fe9b815e962a80
shotasan <wondermoai@gmail.com>
2020-07-25 23:08:05 +0900

    post.php

---
+++ src/chap8/post1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="post2.php" method="POST">
+    <label for="name">名前：</label>
+    <input type="text" name="name" id="name" size="15">
+    <input type="submit" value="送信">
+  </form>
+</body>
+</html>
